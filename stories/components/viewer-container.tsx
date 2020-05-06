fd52ecdf00513edc0b80626404e0aeec48c54e73
StuRuby <937931249@qq.com>
2020-05-07 00:59:08 +0800

    feature:测试storybook 初始化组件

---
+++ stories/components/viewer-container.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+export const ViewerContainer = styled.div.attrs({
+	id: 'viewer',
+})`
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	margin: 0;
+	padding: 0;
+`;
