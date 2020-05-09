bdcb2bf2c4cd92b4937b80999660dfa9572ac603
StuRuby <937931249@qq.com>
2020-05-09 14:10:16 +0800

    feature:添加3D api

---
+++ src/api/overlay/div-icon.ts
@@ -0,0 +1,31 @@
+class DivIcon {
+    constructor(position: any, div: string | HTMLElement) { }
+    /**
+     * 添加css类名
+     */
+    addClass() { }
+    /**
+     * 添加到`viewer`
+     */
+    addToViewer() { }
+    /**
+     * 删除
+     */
+    remove() { }
+    /**
+     * 显示
+     */
+    show() { }
+    /**
+     * 隐藏
+     */
+    hide() { }
+    /**
+     * 绑定事件
+     */
+    on() { }
+    /**
+     * 解绑事件
+     */
+    off() { }
+}
---
+++ src/api/overlay/model.ts
@@ -0,0 +1,31 @@
+class Model {
+    constructor(position: any, url: string) { }
+    /**
+     * 设置模型样式
+     */
+    setStyle() { }
+    /**
+     * 添加到`viewer`
+     */
+    addToViewer() { }
+    /**
+     * 删除
+     */
+    remove() { }
+    /**
+     * 显示
+     */
+    show() { }
+    /**
+     * 隐藏
+     */
+    hide() { }
+    /**
+     * 绑定事件
+     */
+    on() { }
+    /**
+     * 解绑事件
+     */
+    off() { }
+}
